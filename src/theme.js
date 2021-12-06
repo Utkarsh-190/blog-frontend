2151a28836511ceb6c7931ca57c886638e23c37d
Utkarsh_190 <utkarshtyagi62@gmail.com>
2021-12-06 23:30:28 +0530

    created mui theme for the app

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    main: "#764abc",
+  },
+  background: {
+    default: "#fff",
+  },
+});
+
+export default theme;
